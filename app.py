8a655b8e85798fe75fd010383ac7ee8b9ed28378
Madhu Charan <62477860+madhucharan@users.noreply.github.com>
2021-03-27 23:10:06 +0530

    add fertilizer recommendation page

--- app.py
+++ app.py
@@ -29,7 +29,23 @@ def predict():
 	else:
 		return ('No model here to use')
     
+@app.route('/fertrec', methods=['POST','GET'])
+def predict():
+	lr = joblib.load("Models/fertilizerrecommendation.pkl")
+	if lr:
+		try:
+			json = request.get_json()	 
+			model_columns = joblib.load("Models/fertilizerrec_columns.pkl")
+			temp=list(json[0].values())
+			vals=np.array(temp)
+			prediction = lr.predict(temp)
+			print("here:",prediction)        
+			return jsonify({'prediction': str(prediction[0])})
 
+		except:        
+			return jsonify({'trace': traceback.format_exc()})
+	else:
+		return ('No model here to use')
 
 if __name__ == '__main__':
     app.run(debug=True)
